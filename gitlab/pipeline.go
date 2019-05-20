d80e6a5aa93c1ab76d13f9de9690787c8e483f69
Simon Ganon <simon@blueboard.io>
2019-05-20 23:57:58 +0200

    fix: typo and add comment

--- gitlab/pipeline.go
+++ gitlab/pipeline.go
@@ -1,11 +1,13 @@
 package gitlab
 
-type PiepelineID int
+// PipelineID is the unique id of a CI pipeline
+type PipelineID int
 
+// Pipeline is the representation of a CI pipeline
 type Pipeline struct {
-	ID     PiepelineID `json:"id"`
-	Status string      `json:"status"`
-	Ref    string      `json:"ref"`
-	SHA    string      `json:"sha"`
-	WebURL string      `json:"web_url"`
+	ID     PipelineID `json:"id"`
+	Status string     `json:"status"`
+	Ref    string     `json:"ref"`
+	SHA    string     `json:"sha"`
+	WebURL string     `json:"web_url"`
 }
